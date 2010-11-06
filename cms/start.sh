e984f82c85def3d70b299c5d9b0934c6603a2c76
jjoachimsthal <jjoachimsthal@ba5dc400-1776-4475-a4ab-d72ee5750016>
2010-11-06 10:00:46 +0000

    more clean up

--- cms/start.sh
+++
@@ -1,3 +0,0 @@
-MAVEN_OPTS="-Xms128m -Xmx256m -XX:PermSize=128m" 
-export MAVEN_OPTS
-nohup mvn clean jetty:run-war -DskipTests -Drepo.path=/home/jasha/repos/repo003 &
--- site/start.sh
+++
@@ -1,1 +0,0 @@
-nohup mvn clean jetty:run -DskipTests &

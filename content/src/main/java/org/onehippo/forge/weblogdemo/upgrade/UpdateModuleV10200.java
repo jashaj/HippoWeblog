a374bb5371cd8848f2c3cced2e276ad241f782c1
Jasha Joachimsthal <j.joachimsthal@onehippo.com>
2011-10-08 12:09:26 +0200

    Add missing properties for the workflow

--- content/src/main/java/org/onehippo/forge/weblogdemo/upgrade/UpdateModuleV10200.java
+++ content/src/main/java/org/onehippo/forge/weblogdemo/upgrade/UpdateModuleV10200.java
@@ -20,98 +20,113 @@ import org.hippoecm.repository.ext.UpdaterModule;
  */
 public class UpdateModuleV10200 implements UpdaterModule {
 
-  @Override
-  public void register(UpdaterContext updaterContext) {
-    updaterContext.registerName("weblogdemo-upgrade-v102a");
-    updaterContext.registerStartTag("weblogdemo-v101b");
-    updaterContext.registerEndTag("weblogdemo-v102a");
-    updaterContext.registerAfter("v19a");
+    @Override
+    public void register(UpdaterContext updaterContext) {
+        updaterContext.registerName("weblogdemo-upgrade-v102a");
+        updaterContext.registerStartTag("weblogdemo-v101b");
+        updaterContext.registerEndTag("weblogdemo-v102a");
+        updaterContext.registerAfter("v19a");
 
-    removeOldInitializers(updaterContext);
-    removeFacetSearchTags(updaterContext);
-    updateGalleryType(updaterContext);
-  }
-
-  /**
-   * Removes initialize nodes that should no longer be used
-   *
-   * @param updaterContext {@link UpdaterContext}
-   */
-  private void removeOldInitializers(UpdaterContext updaterContext) {
-    updaterContext.registerVisitor(new UpdaterItemVisitor.PathVisitor("/hippo:configuration/hippo:initialize") {
-      @Override
-      protected void leaving(Node node, int level) throws RepositoryException {
-        removeChildNodeIfExists(node, "hst-live");
-        removeChildNodeIfExists(node, "hst-preview");
-        removeChildNodeIfExists(node, "tagging-facetsearch");
-        removeChildNodeIfExists(node, "custom-browserPerspective");
-        removeChildNodeIfExists(node, "virtualhosts");
-        removeChildNodeIfExists(node, "custom-thumbnail-size");
-
-      }
-    });
-  }
-
-  /**
-   * Updates existing content to use the new hippogallery:imageset
-   *
-   * @param updaterContext {@link UpdaterContext}
-   */
-  private void updateGalleryType(UpdaterContext updaterContext) {
-    updaterContext.registerVisitor(new UpdaterItemVisitor.PathVisitor("/content/gallery") {
-      @Override
-      protected void leaving(Node node, int level) throws RepositoryException {
-        if (!node.hasProperty("hippostd:gallerytype")) {
-          return;
-        }
-        Property property = node.getProperty("hippostd:gallerytype");
-        Value[] values = property.getValues();
-        List<Value> newValues = new ArrayList<Value>();
-        for (Value value : values) {
-          if ("hippogallery:exampleImageSet".equals(value)) {
-            newValues.add(new StringValue("hippogallery:imageset"));
-          } else {
-            newValues.add(value);
-          }
-        }
-        property.setValue(newValues.toArray(new Value[newValues.size()]));
-      }
-    });
-
-    updaterContext.registerVisitor(
-            new UpdaterItemVisitor.QueryVisitor("//content/documents//*[@jcr:primaryType='hippostd:html']",
-                    Query.XPATH) {
-              @Override
-              protected void leaving(Node node, int level) throws RepositoryException {
-                Property property = node.getProperty("hippostd:content");
-
-                String value = property.getString();
-                if (value != null) {
-                  String newValue = value.replaceAll("hippogallery:picture", "hippogallery:original");
-                  property.setValue(newValue);
-                }
-              }
-            });
-  }
-
-  /**
-   * Deletes the existing /tags Node which is changed from type facetsearch to facetselect
-   *
-   * @param updaterContext {@link UpdaterContext}
-   */
-  private void removeFacetSearchTags(UpdaterContext updaterContext) {
-    updaterContext.registerVisitor(new UpdaterItemVisitor.PathVisitor("/tags") {
-      @Override
-      protected void leaving(Node node, int level) throws RepositoryException {
-        node.remove();
-      }
-    });
-  }
-
-  private void removeChildNodeIfExists(Node node, String childToDelete) throws RepositoryException {
-    if (node.hasNode(childToDelete)) {
-      Node removeChild = node.getNode(childToDelete);
-      removeChild.remove();
+        removeCustomNewBlogFolderStructureQueries(updaterContext);
+        removeOldInitializers(updaterContext);
+        removeFacetSearchTags(updaterContext);
+        updateGalleryType(updaterContext);
     }
-  }
+
+    private void removeCustomNewBlogFolderStructureQueries(UpdaterContext updaterContext) {
+        updaterContext.registerVisitor(
+                new UpdaterItemVisitor.PathVisitor("/hippo:configuration/hippo:queries/hippo:templates") {
+            @Override
+            protected void leaving(Node node, int level) throws RepositoryException {
+                removeChildNodeIfExists(node, "new-year-folder");
+                removeChildNodeIfExists(node, "new-month-folder");
+                removeChildNodeIfExists(node, "new-blogpost");
+            }
+        });
+    }
+
+    /**
+     * Removes initialize nodes that should no longer be used
+     *
+     * @param updaterContext {@link UpdaterContext}
+     */
+    private void removeOldInitializers(UpdaterContext updaterContext) {
+        updaterContext.registerVisitor(new UpdaterItemVisitor.PathVisitor("/hippo:configuration/hippo:initialize") {
+            @Override
+            protected void leaving(Node node, int level) throws RepositoryException {
+                removeChildNodeIfExists(node, "hst-live");
+                removeChildNodeIfExists(node, "hst-preview");
+                removeChildNodeIfExists(node, "tagging-facetsearch");
+                removeChildNodeIfExists(node, "custom-browserPerspective");
+                removeChildNodeIfExists(node, "virtualhosts");
+                removeChildNodeIfExists(node, "custom-thumbnail-size");
+                removeChildNodeIfExists(node, "queries-new-year-folder");
+                removeChildNodeIfExists(node, "queries-new-month-folder");
+                removeChildNodeIfExists(node, "queries-new-blogpost");
+            }
+        });
+    }
+
+    /**
+     * Updates existing content to use the new hippogallery:imageset
+     *
+     * @param updaterContext {@link UpdaterContext}
+     */
+    private void updateGalleryType(UpdaterContext updaterContext) {
+        updaterContext.registerVisitor(new UpdaterItemVisitor.PathVisitor("/content/gallery") {
+            @Override
+            protected void leaving(Node node, int level) throws RepositoryException {
+                if (!node.hasProperty("hippostd:gallerytype")) {
+                    return;
+                }
+                Property property = node.getProperty("hippostd:gallerytype");
+                Value[] values = property.getValues();
+                List<Value> newValues = new ArrayList<Value>();
+                for (Value value : values) {
+                    if ("hippogallery:exampleImageSet".equals(value)) {
+                        newValues.add(new StringValue("hippogallery:imageset"));
+                    } else {
+                        newValues.add(value);
+                    }
+                }
+                property.setValue(newValues.toArray(new Value[newValues.size()]));
+            }
+        });
+
+        updaterContext.registerVisitor(
+                new UpdaterItemVisitor.QueryVisitor("//content/documents//*[@jcr:primaryType='hippostd:html']",
+                        Query.XPATH) {
+                    @Override
+                    protected void leaving(Node node, int level) throws RepositoryException {
+                        Property property = node.getProperty("hippostd:content");
+
+                        String value = property.getString();
+                        if (value != null) {
+                            String newValue = value.replaceAll("hippogallery:picture", "hippogallery:original");
+                            property.setValue(newValue);
+                        }
+                    }
+                });
+    }
+
+    /**
+     * Deletes the existing /tags Node which is changed from type facetsearch to facetselect
+     *
+     * @param updaterContext {@link UpdaterContext}
+     */
+    private void removeFacetSearchTags(UpdaterContext updaterContext) {
+        updaterContext.registerVisitor(new UpdaterItemVisitor.PathVisitor("/tags") {
+            @Override
+            protected void leaving(Node node, int level) throws RepositoryException {
+                node.remove();
+            }
+        });
+    }
+
+    private void removeChildNodeIfExists(Node node, String childToDelete) throws RepositoryException {
+        if (node.hasNode(childToDelete)) {
+            Node removeChild = node.getNode(childToDelete);
+            removeChild.remove();
+        }
+    }
 }
